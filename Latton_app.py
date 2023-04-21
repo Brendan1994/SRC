ce5c11183103dfcd7773b77db33774181a7483bb
Brendan1994 <129877936+Brendan1994@users.noreply.github.com>
2023-04-21 09:19:05 +0100

    Update Latton_app.py
    Amend URL

--- Latton_app.py
+++ Latton_app.py
@@ -5,7 +5,7 @@ import requests as rq
 st.title("Latton TT Series")
 
 def Get_Results():
-    Results = rq.get(https://www.swindon-rc.co.uk/index.php/component/content/article/104)
+    Results = rq.get("https://www.swindon-rc.co.uk/index.php/component/content/article/104")
     Normalized_Results = pandas.json_normalize(Results.json())
     return Normalized_Results
 
