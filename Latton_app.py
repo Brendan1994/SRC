30fc77ea5678686e937a911970c9794a4d277b1f
Brendan1994 <129877936+Brendan1994@users.noreply.github.com>
2023-04-21 09:15:15 +0100

    Update Latton_app.py
    Request results from club site and push into a dataframe

--- Latton_app.py
+++ Latton_app.py
@@ -2,4 +2,11 @@ import streamlit as st
 import pandas as pd
 import requests as rq
 
-st.header("Latton TT Series")
+st.title("Latton TT Series")
+
+def Get_Results():
+    Results = rq.get(https://www.swindon-rc.co.uk/index.php/component/content/article/104)
+    Normalized_Results = pandas.json_normalize(Results.json())
+    return Normalized_Results
+
+st.dataframe(Get_Results)  
