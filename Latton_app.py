4f78d08a3e940b4588a5cd3f0b7a67e7f190ba43
Brendan1994 <129877936+Brendan1994@users.noreply.github.com>
2023-04-24 14:13:23 +0100

    Update Latton_app.py
    Insert Data Scrape query SRC_Data_Scrape_v2

--- Latton_app.py
+++ Latton_app.py
@@ -1,12 +1,50 @@
 import streamlit as st
 import pandas as pd
 import requests as rq
+from datetime import datetime
+import re
 
 st.title("Latton TT Series")
 
-def Get_Results():
-    Results = rq.get("https://www.swindon-rc.co.uk/index.php/component/content/article/104", verify=False)
-    Normalized_Results = pandas.json_normalize(Results.json())
-    return Normalized_Results
+#def Get_Results():
+#    Results = rq.get("https://www.swindon-rc.co.uk/index.php/component/content/article/104", verify=False)
+#    Normalized_Results = pandas.json_normalize(Results.json())
+#    return Normalized_Results
 
-st.dataframe(Get_Results)  
+#st.dataframe(Get_Results)  
+
+#Create list of dataframes - 1 html table per df
+df = pd.read_html("https://www.swindon-rc.co.uk/index.php/component/content/article/104",header=0)
+
+#Add 'Week' column to each df
+#f = rq.get("https://www.swindon-rc.co.uk/index.php/component/content/article/104")
+f = open('2022_Time_Trial_Results.html','r')
+
+Week = []
+#lines = f.text.readlines()
+lines = f.readlines()
+
+for line in lines:
+    if re.search('<strong style="color: #666666; font-family: Arial, Helvetica, sans-serif; font-size: 16px;">',line):
+        line = re.sub('<(.*?)\>','',line)
+        line = re.sub('&nbsp;',' ',line)
+        line = re.sub("New Year's Day ",'1 January ',line)
+        #print(line)
+        try:
+            d = datetime.strptime(line.strip(),'%d %B %Y')
+            #print(d)
+            Week.append(d)
+        except:
+            continue
+  
+#Append df's into single df
+df1 = pd.DataFrame()
+
+for idx,a in enumerate(df):
+    df[idx]['Week'] = Week[idx]
+    df1 = pd.concat([df[idx],df1])
+ df1.reset_index(inplace=True)
+
+st.dataframe(df1)
+
+   
