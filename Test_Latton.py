5d23562ffdaca382144309a2cf23dc67e81ad5e2
Brendan1994 <129877936+Brendan1994@users.noreply.github.com>
2023-05-16 14:38:24 +0100

    Update Test_Latton.py
    Moved to xlsx file as data source rather than scraping off of the website

--- Test_Latton.py
+++ Test_Latton.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import requests as rq
+#import requests as rq
 from datetime import datetime
-import re
-from lxml import html
+#import re
+#from lxml import html
 
 #set wide mode
 st.set_page_config(layout="wide")
@@ -12,50 +12,62 @@ st.set_page_config(layout="wide")
 st.title("Latton TT Series") 
 
 #Create list of dataframes - 1 html table per df
-df = pd.read_html("https://www.swindon-rc.co.uk/index.php/component/content/article/104",header=0)
+#df = pd.read_html("https://www.swindon-rc.co.uk/index.php/component/content/article/104",header=0)
+    #We are now creating a df using an excel spreadsheet
+    df = pd.read_excel("Results Archive 11-5-23.xlsx")
+    df = df[['Position','Start Number','Name','Club','Split Time','Time']]
 
 #Add 'Week' column to each df
-f = rq.get("https://www.swindon-rc.co.uk/index.php/component/content/article/104")
+#f = rq.get("https://www.swindon-rc.co.uk/index.php/component/content/article/104")
 #f = open('2022_Time_Trial_Results.html','r')
+    #Iterate over the DataFrame, where the 'Name' column is date define 'date' variable and append this variable into a new 'Date' column
+    for index, row in df.iterrows():
+        if type(row['Name']) == pd._libs.tslibs.timestamps.Timestamp:
+            date = row['Name']
+        df.at[index,'Date'] = date
+
+    #Add new column with the day of the race
+    df['Day'] = df['Date'].dt.day_name()
 
 
-Week = []
+
+#Week = []
 
 #lines = f.text.readlines()
 
 
-for line in lines:
-    if re.search('<strong style="color: #666666; font-family: Arial, Helvetica, sans-serif; font-size: 16px;">',line):
-        line = re.sub('<(.*?)\>','',line)
-        line = re.sub('&nbsp;',' ',line)
-        line = re.sub("New Year's Day ",'1 January ',line)
+#for line in lines:
+#    if re.search('<strong style="color: #666666; font-family: Arial, Helvetica, sans-serif; font-size: 16px;">',line):
+#        line = re.sub('<(.*?)\>','',line)
+#        line = re.sub('&nbsp;',' ',line)
+#        line = re.sub("New Year's Day ",'1 January ',line)
         #print(line)
-        try:
-            d = datetime.strptime(line.strip(),'%d %B %Y')
+#        try:
+#            d = datetime.strptime(line.strip(),'%d %B %Y')
             #print(d)
-            Week.append(d.date())
-        except:
-            continue
+#            Week.append(d.date())
+#        except:
+#            continue
   
 #Append df's into single df
-df1 = pd.DataFrame()
+#df1 = pd.DataFrame()
 
-for idx,a in enumerate(df):
-    df[idx]['Week'] = Week[idx]
-    df1 = pd.concat([df[idx],df1])
+#for idx,a in enumerate(df):
+#    df[idx]['Week'] = Week[idx]
+#    df1 = pd.concat([df[idx],df1])
 
 #Remove index column
-df1.set_index('Position',inplace=True)
+df.set_index('Position',inplace=True)
 
 #Sort Values by Week desc, Position asc
 df1.sort_values(by=["Week","Time"], ascending=[False, True], inplace=True)
 
 #Unique list of racers names
-Names = df1.Name.drop_duplicates()
+Names = df.Name.drop_duplicates()
 Names.sort_values(ascending=True, inplace=True)
 
 #Unique list of Weeks
-Date = df1.Week.drop_duplicates()
+Date = df.Week.drop_duplicates()
 Date.sort_values(ascending=False, inplace=True)
 
 #Present text
@@ -64,14 +76,14 @@ Date = st.multiselect("Enter date to filter the results",list(Date))
 
 #Filter df by input
 if (Racer and Date):
-    rslt_df1 = df1[df1['Week'].isin(Date)]
+    rslt_df1 = df[df['Week'].isin(Date)]
     rslt_df = rslt_df1[rslt_df1['Name'].isin(Racer)]
 elif Racer:
-    rslt_df = df1[df1['Name'].isin(Racer)]
+    rslt_df = df[df['Name'].isin(Racer)]
 elif Date:
-    rslt_df = df1[df1['Week'].isin(Date)]
+    rslt_df = df[df['Week'].isin(Date)]
 else:
-    rslt_df = df1
+    rslt_df = df
 
 #Present filtered df
 st.dataframe(rslt_df)
