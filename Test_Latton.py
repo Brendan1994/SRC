5ec1a6d49b971ce232cfe1ec754e1843c6e2a66e
Brendan1994 <129877936+Brendan1994@users.noreply.github.com>
2023-05-17 08:10:42 +0100

    Update Test_Latton.py

--- Test_Latton.py
+++ Test_Latton.py
@@ -21,17 +21,17 @@ df = df[['Position','Start Number','Name','Club','Split Time','Time']]
 #f = rq.get("https://www.swindon-rc.co.uk/index.php/component/content/article/104")
 #f = open('2022_Time_Trial_Results.html','r')
     #Iterate over the DataFrame, where the 'Name' column is date define 'date' variable and append this variable into a new 'Date' column
-for index, row in df.iterrows():
-    if type(row['Name']) == pd._libs.tslibs.timestamps.Timestamp:
-        date = row['Name']
-        df.at[index,'type'] = 'date'
-    df.at[index,'Date'] = date
+#for index, row in df.iterrows():
+#    if type(row['Name']) == pd._libs.tslibs.timestamps.Timestamp:
+#        date = row['Name']
+#        df.at[index,'type'] = 'date'
+#    df.at[index,'Date'] = date
 
     #Add new column with the day of the race
 #df['Day'] = df['Date'].dt.day_name()
 
     #Remove dates from rows
-df = df[df.type] != 'date'
+#df = df[df.type] != 'date'
 st.dataframe(df)
 #df['Date'] = pd.to_datetime(df['Date'])
 #df['Time'] = pd.to_datetime(df['Time'])
